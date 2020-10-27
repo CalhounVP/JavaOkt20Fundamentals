3869f3261641d6f3f6df836c5055825ab7980a8a
Calhoun VP <55089727+CalhounVP@users.noreply.github.com>
2020-10-27 19:46:46 +0100

    Update Ex4Scrabble.java
    added better comments

--- src/be/intecbrussel/chapter7/Ex4Scrabble.java
+++ src/be/intecbrussel/chapter7/Ex4Scrabble.java
@@ -6,11 +6,8 @@ public class Ex4Scrabble {
     public static void main(String[] args) {
         //properties
         Scanner scanner = new Scanner(System.in);
-        char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
         String text;
-        char[] textIntoChars;
         int totalPoints = 0;
 
         //asking input
@@ -19,20 +16,24 @@ public class Ex4Scrabble {
         scanner.close();
 
         //processing input
-
+        //optimized manner making use of the unicode values of letters
         for (char letter : text.toLowerCase().toCharArray()) {
-            totalPoints += points[(int) letter - 97];
+            totalPoints += points[(int) letter - 97]; //could also do - 'a'
         }
 
         System.out.println(totalPoints);
+        
+        //resetting points to 0 purely to demonstrate a different less optimized approach
+        char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         totalPoints =0;
-
-
-        textIntoChars = text.toLowerCase().toCharArray();
-
-        for (char letter: textIntoChars) {
+        
+        //loop through every letter of the input
+        for (char letter: text.toLowerCase().toCharArray()) {
+            //find the i value in the letters array for this char
             for (int i = 0; i < letters.length; i++) {
                 if (letters[i] == letter) {
+                    //the score will be on the same i location in the score array
                     totalPoints += points[i];
                     break;
                 }
